6ddb0844a2ce5d20625c8945916e5065d85dd558
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2015-03-13 11:03:41 -0400

    base idea.

--- src/firstcommit.js
+++ src/firstcommit.js
@@ -1,3 +1,13 @@
 /**
  * Created by wilson.campusano on 3/13/2015.
  */
+
+var configuration ={
+	events :['',''],
+	inputsFields:[ {
+		input:'blah',
+		formatAttached: 'bleh'
+	}, ],
+	format: ''
+};
+$.partialFormat(configuration);
